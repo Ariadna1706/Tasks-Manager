096aad8cc41fa4918415560ae9e4a5b0f61c8558
Ariadna Nicieja <ariadna.nicieja@gmail.com>
2023-06-02 13:48:57 +0200

    commit 2

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -5,11 +5,13 @@ class TasksManager extends React.Component {
   state = {
     task: "",
     time: 0,
-    isRunning: null,
+    isRunning: false,
     isDone: null,
     isRemoved: false,
     tasks: [],
   };
+
+  updateDatabase = () => {};
 
   onClick = () => {
     const { tasks } = this.state;
@@ -39,14 +41,19 @@ class TasksManager extends React.Component {
       headers: { "Content-Type": "application/json" },
     };
     fetch(this.apiUrl, options)
-      .then((resp) => console.log(resp))
-      .catch((err) => console.error(err))
-      .finally(this.renderTasks(this.state.tasks));
+      .then(() => {
+        this.setState({ tasks: [...this.state.tasks, data] });
+      })
+      .catch((err) => console.error("Error" + err));
+    this.setState({
+      task: "",
+    });
   };
 
   render() {
     const { tasks } = this.state;
-    console.log(tasks);
+
+   
 
     return (
       <section>
@@ -65,22 +72,124 @@ class TasksManager extends React.Component {
   }
 
   renderTasks(tasks) {
-    return tasks.map((item) => {
+    return tasks.map((task) => {
       return (
         <section>
           <header>
-            {" "}
-            {item.name}, time: {item.time}
+            {task.name}, time: {task.time}
           </header>
           <footer>
-            <button>start/stop</button>
-            <button>zakończone</button>
-            <button disabled="true">usuń</button>
+            <button
+              onClick={() => {
+                task.isRunning === false
+                  ? this.startTimer(task.id)
+                  : this.stopTimer(task.id);
+              }}
+            >
+              start/stop
+            </button>
+            <button
+              onClick={() => {
+                task.isDone === null || task.isRunning === true
+                  ? this.endTask(task.id) 
+                  : console.log("działa");
+              }}
+            >
+              zakończone
+            </button>
+            <button  disabled="true">usuń</button>
           </footer>
         </section>
       );
     });
   }
+
+  endTask = (id) => {
+    this.stopTimer(id);
+    this.setState((state) => {
+      const newTasks = state.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            time: task.time,
+            isDone: true,
+            isRunning: false,
+          };
+        }
+        return task;
+      });
+
+      return {
+        tasks: newTasks.sort(function (a, b) {
+          if (a.isDone < b.isDone) {
+            return 1;
+          }
+        }),
+      };
+    });
+    this.updateData();
+  };
+
+  startTimer = (id) => {
+    this.IntervalId = setInterval(() => {
+      this.setState((state) => {
+        const newTasks = state.tasks.map((task) => {
+          if (task.id === id) {
+            return { ...task, time: task.time + 1, isRunning: true };
+          }
+          return task;
+        });
+
+        return {
+          tasks: newTasks,
+        };
+      });
+    }, 1000);
+    this.updateData();
+  };
+
+  stopTimer = (id) => {
+    this.setState((state) => {
+      clearInterval(this.IntervalId);
+      const newTasks = state.tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, isRunning: false };
+        }
+        return task;
+      });
+
+      return {
+        tasks: newTasks,
+      };
+    });
+    this.updateData();
+  };
+
+  updateData = () => {
+    const { tasks } = this.state;
+
+    tasks.map((task) => {
+      const data = {
+        name: task.name,
+        time: task.time,
+        isRunning: task.isRunning,
+        isDone: task.isDone,
+        isRemoved: task.isRemoved,
+      };
+
+      console.log(data);
+
+      const options = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      };
+
+      fetch(`${this.apiUrl}/${task.id}`, options).then((resp) =>
+        console.log(resp)
+      );
+    });
+  };
 
   componentDidMount() {
     fetch(this.apiUrl)
@@ -91,29 +200,15 @@ class TasksManager extends React.Component {
         return Promise.reject(resp);
       })
       .then((object) => {
-        this.setState({
-          tasks: object,
-        });
+        this.setState({ tasks: object });
       })
       .catch((err) => console.error(err));
   }
 
-  componentDidUpdate() {
-    /*  return fetch(this.apiUrl)
-     .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        }
-        return Promise.reject(resp);
-      })
-      .then((object) => {
-        this.setState({
-          tasks: object,
-        });
-      })
-      .catch((err) => console.error(err));*/
+  componentDidUpdate() {}
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
-  componentWillUnmount() {}
 }
 
 export default TasksManager;
